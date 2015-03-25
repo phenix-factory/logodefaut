96472611570d9292e81a3524d54d5230dc6ad811
Debondt Didier <p@henix.be>
2015-03-25 18:50:45 +0100

    Gestion du logo du site comme logo par défaut

--- logodefaut_fonctions.php
+++ logodefaut_fonctions.php
@@ -20,6 +20,15 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  */
 function logo_par_defaut($logo) {
     if (empty($logo)) {
+        include_spip('inc/config');
+        $config = lire_config('logodefaut');
+
+        // Si la config demande le SPIP logo commen logo par défaut, on l'utilise
+        if (isset($config['logo_du_site']) and $config['logo_du_site']) {
+            $chercher_logo = charger_fonction('chercher_logo', 'inc');
+            $logo_par_defaut = $chercher_logo(0, 'site');
+            $logo_par_defaut = $logo_par_defaut[0];
+        }
         $balise_img = charger_filtre('balise_img');
         $logo = $balise_img(find_in_path('img/logo_defaut.png'), '', 'spip_logos');
     }
