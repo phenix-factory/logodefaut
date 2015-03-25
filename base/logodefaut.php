a1cdfcad2b75e6d3bd76d909470661a6c612ab60
Debondt Didier <p@henix.be>
2015-03-25 18:51:40 +0100

    Activer la gestion des objets via le formulaire de config

--- base/logodefaut.php
+++ base/logodefaut.php
@@ -13,12 +13,17 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  */
 function logodefaut_declarer_tables_interfaces($interfaces) {
 
-    $logo_par_defaut = array(
-        'LOGO_INSTITUTION',
-        'LOGO_EVENT',
-        'LOGO_AUTEUR'
-    );
-    foreach($logo_par_defaut as $logo)
+    include_spip('inc/config');
+    $config = lire_config('logodefaut');
+
+    // On va créer la liste des objets qui sont affecté
+    $logo_par_defaut_objet = array();
+    foreach($config['objet'] as $table_objet) {
+        if (!empty($table_objet))
+            $logo_par_defaut_objet[] = 'LOGO_'.strtoupper(objet_type($table_objet));
+    }
+
+    foreach($logo_par_defaut_objet as $logo)
         $interfaces['table_des_traitements'][$logo][] = 'logo_par_defaut(%s)';
 
     return $interfaces;
